a3e12ab5ca12165bf7ba8b4ce1009d4f484110c7
Felix Gomez <frg0801@gmail.com>
2018-12-07 09:59:03 -0800

    Obtención de los nombres de tablas y almacenamiento en variables y listas

--- parser.py
+++ parser.py
@@ -10,15 +10,41 @@ sql="SELECT S.*, P.*, C.*, CO.*, T.* "\
 		"INNER JOIN TOPIC T "\
 			"ON T.TOPIC = CO.TOPIC;"
 
+columnname = "SELECT column_name"\
+				" FROM information_schema.columns"\
+				" WHERE table_schema = 'public'"\
+				" AND table_name = "
 
 def parser(sql):
-	print(sql)
-	posicion_from = sql.find("FROM")
-	posicion_tablabase = posicion_from + 37
+	#Se separa el string SQL por el token "espacio" en una lista
+	sql_list = sql.split(' ')
+	print(sql_list)
+	print("\n")
 
+	# Se obtiene el nombre de la TABLA BASE	
+	a = 0
+	for i in sql_list :
+		if i == 'FROM':
+			tabla_base = sql_list[a+1]
+			tabla_base_apodo = sql_list[a+2]
+			break
+		a = a+1
 
+	# Se obtiene el nombre de las demas tablas
+	# Se almacena en una lista, se guarda NOMBRE, APODO
+	a = 0
+	list_tablas = []
+	for i in sql_list :
+		if i == 'JOIN':
+			list_tablas.append(sql_list[a+1])
+			list_tablas.append(sql_list[a+2])
+		a = a+1
 
+	print("Tabla base: " + tabla_base + ", " +tabla_base_apodo + "\n")
+	print("Lista de tablas:")
+	print(list_tablas)
 
+#Se ejecuta la función PARSER
 parser(sql)
 
 #"adams";"srw";"adams";"researcher";"scuf";"no";23;"scuf";"university";"srw";3;"advanced";"ilp";"ilp";f;"muggleton"
